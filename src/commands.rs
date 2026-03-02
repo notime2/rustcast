8d9a5c6761eec5a4322d1329c468eec450c62c10
unsecretised <no1umang@gmail.com>
2026-03-02 21:42:58 +0800

    Emergency fix for shell commands

--- src/commands.rs
+++ src/commands.rs
@@ -37,7 +37,7 @@ impl Function {
             Function::RunShellCommand(command) => {
                 Command::new("sh")
                     .arg("-c")
-                    .arg(format!("\"{}\"", command))
+                    .arg(format!("{}", command))
                     .spawn()
                     .ok();
             }
